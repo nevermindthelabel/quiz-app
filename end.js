3c2d9bd5657d9a179ef69a59c8d330d6c29d030f
Matt Kilcup <nevermindthelabel@gmail.com>
2019-11-28 13:47:55 -0700

    added logic to get score from local storage and display on screen

--- end.js
+++ end.js
@@ -1,8 +1,12 @@
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('Most Recent Score');
+
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
-  saveScoreBtn.disabled = !username.value;
+  saveScoreBtn.disabled = !username.value.length;
 });
 
 
