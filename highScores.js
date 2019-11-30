8d91e8a8e7638525986b4aa57833d83168a68e6d
Matt Kilcup <nevermindthelabel@gmail.com>
2019-11-29 22:22:12 -0700

    added logic for mapping over local storage array

--- highScores.js
+++ highScores.js
@@ -1,2 +1,6 @@
 const highScoresList = document.getElementById('highScoresList');
+const highScores = JSON.parse(localStorage.getItem('High Scores')) || [];
 
+highScores.map(score => {
+  console.log(`${score.name} : ${score.score}`);
+});
