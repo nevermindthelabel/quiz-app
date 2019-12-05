91d5347c1af288e8cc2ce0d45d651fe58fc71cd6
Matt Kilcup <nevermindthelabel@gmail.com>
2019-12-04 21:56:44 -0700

    completed the api call for trivia data

--- game.js
+++ game.js
@@ -16,7 +16,18 @@ fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=mul
         .then(res => {
   return res.json();
 }).then( loadedQuestions => {
-  questions = loadedQuestions;
+  questions = loadedQuestions.results.map(loadedQuestion => {
+    const formattedQuestion = {
+      question: loadedQuestion.question
+    };
+    const answerChoices = [...loadedQuestion.incorrect_answers];
+    formattedQuestion.answer = Math.floor(Math.random() * 3) +1;
+    answerChoices.splice(formattedQuestion.answer -1, 0, loadedQuestion.correct_answer);
+    answerChoices.forEach((choice, i) => {
+      formattedQuestion['choice' + (i + 1)] = choice;
+    })
+    return formattedQuestion;
+  })
   startGame();
 })
 .catch(err => {
